e9b31a736cfeb4710686fa556e674d481f92a6dc
sofiane <sofiane162@hotmail.fr>
2024-10-13 19:39:17 +0200

    Ajout d'une route GET
    Cette route permet de gérer toutes les requêtes et de monter l'application React en renvoyant la vue Blade correspondante.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{any}', function () {
+    return view('app'); // Renvoie la vue Blade "app.blade.php" où React est monté
+})->where('any', '.*');
