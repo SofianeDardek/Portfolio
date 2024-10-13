5d616943be27d85e324c86bfb06f2648738e8641
sofiane <sofiane162@hotmail.fr>
2024-10-13 19:37:03 +0200

    Ajout de code
    - Ajout d'un conteneur div avec l'id "app" dans le fichier HTML pour le point d'entrée de l'application.
    
    - Intégration de Vite avec la directive @vite('resources/js/main.jsx') pour charger le script JavaScript principal.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/js/main.jsx')
 </head>
 <body>
-    <h1>Test</h1>
+    <div id="app"></div>
 </body>
 </html>
