77e36534f76f9ec942cfe95e016a4f7c1f52f93a
sofiane <sofiane162@hotmail.fr>
2024-10-19 20:21:42 +0200

    Config router avec routes
    - Ajout de la configuration du router avec les routes "skills", "projects" et "contact"

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,12 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
+import Navbar from './components/Navbar';
+import { createBrowserRouter, Outlet, RouterProvider, } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Navbar />,
+    children : [
+      {
+        path : "skills",
+        element : <h1>competences</h1>
+      },
+      {
+        path : "projects",
+        element : <h1>projets</h1>
+      },
+      {
+        path : "contact",
+        element : <h1>contact</h1>
+      }      
+    ]
+  }
+
+
+]);
 
 
 createRoot(document.getElementById('app')).render(
   <StrictMode>
-     <>
-    <h1>testt</h1>
-    </>
+    <RouterProvider router={router} />
   </StrictMode>
 )
