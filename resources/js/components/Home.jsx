1685cced8dd89acf3ee00784d0fa59788a58826b
sofiane <sofiane162@hotmail.fr>
2024-10-21 19:10:22 +0200

    Ajout de texte Lorem
    - Ajout de texte pour voir le rendu

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -1,7 +1,15 @@
 function Home(){
 
     return <>
-        <h1>Accueil components</h1>
+    <div className="content-home">
+        <h1 className="title-page">Accueil</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, nam rerum veritatis eius beatae consequatur quisquam qui tenetur atque voluptates voluptatum impedit molestiae. Veritatis eaque ipsum debitis dolor quam dolore!
+        Officiis dolorem quasi placeat debitis ex iste voluptatibus quia aspernatur! Nobis recusandae ratione illum maxime odio! Enim nulla rem porro, facere nobis expedita consequuntur, dicta corporis incidunt, at et autem!
+        Debitis ipsum saepe nulla, alias ratione quibusdam ipsam rerum, quas soluta, totam maxime temporibus possimus corrupti? Ipsum corporis quas eos tenetur vero aspernatur, a dicta possimus iusto animi quod perspiciatis.
+        Sed iure, atque incidunt, facilis perferendis perspiciatis quae dicta voluptatum suscipit aspernatur est quo labore quas laudantium doloremque eius commodi numquam dolorum optio dignissimos, modi maxime! Dolore consectetur aliquam hic?</p>
+        <hr />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum unde corporis aliquam, exercitationem natus dolores modi, culpa, expedita reiciendis tempore cum quae ipsam! Enim, molestiae sit rem accusantium illo odio.</p>
+    </div>
     </>
 }
 
