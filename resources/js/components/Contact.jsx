1a1981fcebe323a3febfc8617ebb33eb50f10ca9
sofiane <sofiane162@hotmail.fr>
2024-10-23 13:58:47 +0200

    Ajout du formulaire de contact
    - Création de la div `box-contact` avec un titre "Contact"
    - Ajout du formulaire `form-contact` comprenant les champs suivants :
      - Groupe `name-group` pour aligner côte à côte les champs "Nom" et "Prénom"
      - Champ email avec le label correspondant
      - Champ message avec un textarea

--- resources/js/components/Contact.jsx
+++ resources/js/components/Contact.jsx
@@ -1,6 +1,27 @@
 function Contact(){
     return <>
-        <h1>Contact Page</h1>
+    <div className="box-contact">
+        <h1>Contact</h1>
+        <form className="form-contact">
+            <div className="name-group">
+                <div>
+                    <label htmlFor="firstName">Nom :</label>
+                    <input name="firstName" type="text" />
+                </div>
+                
+                <div>
+                    <label htmlFor="name">Prenom :</label>
+                    <input type="text" name="name" />
+                </div>
+            </div>
+
+            <label htmlFor="email">Email :</label>
+            <input type="text" name="email" />
+
+            <label htmlFor="message">Message :</label>
+            <textarea name="message" id=""></textarea>
+        </form>
+    </div>
     </>
 }
 
