4088fe88ca11332f62023e3ab28d5c2640513841
sofiane <sofiane162@hotmail.fr>
2024-10-24 20:07:58 +0200

    Refonte de la section Home
    - Ajout d'une section de liens sociaux avec des icônes pour LinkedIn et GitHub, avec des attributs `target="_blank"` et `rel="noopener noreferrer"` pour la sécurité.
    - Réorganisation de la structure HTML pour intégrer une nouvelle classe `flex-imgProfile-socials` afin d'aligner l'image de profil et les liens sociaux.
    - Mise à jour du titre pour inclure le nom complet et l'âge : "Sofiane Dardek, 24 ans, Développeur Web Passionné".
    - Ajout d'un élément `hr` pour séparer visuellement les sections.
    - Amélioration de l'accessibilité avec des attributs `alt` pour les images.

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -3,11 +3,25 @@ function Home(){
     return <>
     <div className="content-home">
         <h1 className="title-page">Accueil</h1>
+        <hr />
         <div className="img-content">
-            <img src="/images/profile.jpg" />
-            <h1>Sofiane</h1>
-            <p>Je m'appelle Sofiane Dardek et je suis passionné par le développement web et l'informatique. Après avoir obtenu mon BTS Systèmes Numériques option Informatique et Réseaux (SNIR), j'ai renforcé mes compétences dans la programmation, les réseaux et les nouvelles technologies. Motivé par l'innovation et les défis techniques, je m'engage à créer des solutions numériques adaptées aux besoins des utilisateurs. Actuellement, je souhaite poursuivre mes études en licence professionnelle en alternance afin de continuer à me former tout en acquérant de l'expérience en entreprise. Ma détermination et ma curiosité me poussent à toujours apprendre et à m'adapter aux nouvelles tendances du secteur.
-            </p>
+            <div className="flex-imgProfile-socials">
+            <img id="img-profile" src="/images/profile.jpg" />
+                <div className="social-links">
+                    <a href="https://www.linkedin.com/in/ton-profil" target="_blank" rel="noopener noreferrer">
+                        <img src="/images/socials/linkedin.svg" alt="LinkedIn" className="social-icon" />
+                    </a>
+                    <a href="https://github.com/ton-profil" target="_blank" rel="noopener noreferrer">
+                        <img src="/images/socials/github.svg" alt="GitHub" className="social-icon" />
+                    </a>
+                </div>
+            </div>
+            
+            <div className="title-text-container">
+                <h1>Sofiane Dardek, 24 ans, Développeur Web Passionné</h1>
+                <p>Je m'appelle Sofiane Dardek et je suis passionné par le développement web et l'informatique. Après avoir obtenu mon BTS Systèmes Numériques option Informatique et Réseaux (SNIR), j'ai renforcé mes compétences dans la programmation, les réseaux et les nouvelles technologies. Motivé par l'innovation et les défis techniques, je m'engage à créer des solutions numériques adaptées aux besoins des utilisateurs. Actuellement, je souhaite poursuivre mes études en licence professionnelle en alternance afin de continuer à me former tout en acquérant de l'expérience en entreprise. Ma détermination et ma curiosité me poussent à toujours apprendre et à m'adapter aux nouvelles tendances du secteur.
+                </p>
+            </div>
         </div>
         <hr />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum unde corporis aliquam, exercitationem natus dolores modi, culpa, expedita reiciendis tempore cum quae ipsam! Enim, molestiae sit rem accusantium illo odio.</p>
