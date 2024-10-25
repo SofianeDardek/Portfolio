3fd82320e85a0baf3519916a3eae1716c4db781a
sofiane <sofiane162@hotmail.fr>
2024-10-25 18:12:36 +0200

    Ajout du composant Skills
    - Création du composant `Skills` avec un affichage structuré des compétences clés : HTML, CSS, JavaScript, PHP, SQL, Laravel, et Git.
    - Mise en place de sections individuelles pour chaque compétence, avec des descriptions pour clarifier les connaissances et l'expérience associées.
    - Ajout de la structure `skills-container` pour un affichage organisé et clair.

---
+++ resources/js/components/Skills.jsx
@@ -0,0 +1,42 @@
+function Skills() {
+    return (
+        <>
+            <div className="content-skills">
+                <h1 className="title-page">Compétences</h1>
+                <hr />
+                <div className="skills-container">
+                    <div className="skill">
+                        <h2>HTML</h2>
+                        <p>Maîtrise des balises HTML, structuration de contenu web.</p>
+                    </div>
+                    <div className="skill">
+                        <h2>CSS</h2>
+                        <p>Compétences en mise en page, design responsive et animations.</p>
+                    </div>
+                    <div className="skill">
+                        <h2>JavaScript</h2>
+                        <p>Développement d'interactions dynamiques et manipulation du DOM.</p>
+                    </div>
+                    <div className="skill">
+                        <h2>PHP</h2>
+                        <p>Création de scripts côté serveur, gestion de bases de données.</p>
+                    </div>
+                    <div className="skill">
+                        <h2>SQL</h2>
+                        <p>Gestion de bases de données relationnelles et rédaction de requêtes.</p>
+                    </div>
+                    <div className="skill">
+                        <h2>Laravel</h2>
+                        <p>Développement d'applications web avec le framework PHP Laravel.</p>
+                    </div>
+                    <div className="skill">
+                        <h2>Git</h2>
+                        <p>Gestion de versions et collaboration sur des projets de développement.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Skills;
