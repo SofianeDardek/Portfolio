876e5bbda522040c2c7458d3155a5cd98a500524
sofiane <sofiane162@hotmail.fr>
2024-10-29 15:06:49 +0100

    Ajout de la fonctionnalité d'envoi de formulaire
    - Ajout de la fonction `handleSubmit` pour gérer la soumission asynchrone du formulaire de contact.
    - Récupération des données du formulaire (nom, email, message) et envoi au serveur via une requête `POST` avec le token CSRF pour sécuriser la requête.
    - Ajout de messages d'alerte pour informer l'utilisateur du succès ou de l'échec de l'envoi du message.

--- resources/js/components/Contact.jsx
+++ resources/js/components/Contact.jsx
@@ -1,10 +1,51 @@
-function Contact(){
+function Contact() {
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+
+
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const message = formData.get("message");
+
+        try {
+            // Récupère le jeton CSRF depuis la balise meta
+            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+            
+            const response = await fetch("http://localhost:8000/api/contact", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": csrfToken, // Ajout du token CSRF
+                },
+                body: JSON.stringify({ name, email, message }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Réponse du serveur:", data);
+            alert("Votre message a été envoyé avec succès !");
+
+        } catch (error) {
+            console.error("Erreur lors de l'envoi:", error);
+            alert("Une erreur est survenue lors de l'envoi du message.");
+        }
+    };
+
+        // alert(name + " " + email + " " + message);
+
     return <>
     <div className="box-contact">
         <h1>Contact</h1>
-        <form className="form-contact">
-            <label htmlFor="firstName">Nom :</label>
-            <input name="firstName" type="text" />
+        <form onSubmit={handleSubmit} className="form-contact">
+            <label htmlFor="name">Nom :</label>
+            <input name="name" type="text" />
             
 
             <label htmlFor="email">Email :</label>
