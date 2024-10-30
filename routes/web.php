bec1ae654737c2d908c1b5fde3ff247800b56eb0
sofiane <sofiane162@hotmail.fr>
2024-10-30 11:34:52 +0100

    Refactorisation de la route API
    - Suppression du code de test pour l'envoi de messages de contact (route `POST /api/contact` avec fonction anonyme).
    - Configuration de la route `POST /api/contact` pour utiliser la méthode `store` du `ContactController`, afin de centraliser la logique de traitement des contacts.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 Route::get('{any}', function () {
     return view('app'); // Renvoie la vue Blade "app.blade.php" où React est monté
 })->where('any', '.*');
+
+/*
+Route::prefix('api')->group(function () {
+    Route::post('/contact', function (Request $request) {
+        return response()->json(['message' => 'Message reçu avec succès']);
+    });
+});
+*/
+
+
+Route::prefix('api')->group(function () {
+    Route::post('/contact', [ContactController::class, 'store']);
+});
