c807b7829b85919c7379ddfbb96b19cb15eb88a9
sofiane <sofiane162@hotmail.fr>
2024-10-30 22:47:28 +0100

    Amélioration du formulaire de contact
    - Ajout de la gestion des erreurs pour le formulaire de contact, avec affichage des messages d'erreur en cas de validation échouée.
    - Implémentation d'un message de succès pour confirmer l'envoi du message.
    - Réinitialisation du formulaire après une soumission réussie.
    - Suppression des alertes pour une meilleure expérience utilisateur.
    - Utilisation de useState pour gérer les erreurs et le message de succès.

--- resources/js/components/Contact.jsx
+++ resources/js/components/Contact.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 function Contact() {
 
+    const [errors, setErrors] = useState({});
+    const [successMessage, setSuccessMessage] = useState("")
+    console.log(successMessage);
+    // console.log("Voici les erreurs : " + JSON.stringify(errors));
+
     const handleSubmit = async (e) => {
+
         e.preventDefault();
 
         const form = e.target;
@@ -14,8 +22,8 @@ function Contact() {
         try {
             // Récupère le jeton CSRF depuis la balise meta
             const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            
-            const response = await fetch("http://localhost:8000/api/contact", {
+
+            const response = await fetch("/api/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -24,17 +32,31 @@ function Contact() {
                 body: JSON.stringify({ name, email, message }),
             });
 
-            if (!response.ok) {
-                throw new Error(`Erreur HTTP! Statut: ${response.status}`);
+            if (response.ok) {
+                // Si la soumission est réussie
+                const data = await response.json();
+                setSuccessMessage("Votre message a été envoyé avec succès !");
+                setErrors({});
+                form.reset();
+            } else {
+                if (response.status === 422) {
+                    setSuccessMessage("");
+                    console.log("erreur de validation");
+                    const data = await response.json();
+                    setErrors(data.errors);
+                } else if (response.status === 500) {
+                    // Gestion d'une erreur serveur
+                    throw new Error("Erreur interne du serveur !");
+                } else {
+                    throw new Error("Erreur lors de la soumission du formulaire");
+                }
             }
 
-            const data = await response.json();
-            console.log("Réponse du serveur:", data);
-            alert("Votre message a été envoyé avec succès !");
+ 
 
         } catch (error) {
             console.error("Erreur lors de l'envoi:", error);
-            alert("Une erreur est survenue lors de l'envoi du message.");
+            //alert("Une erreur est survenue lors de l'envoi du message.");
         }
     };
 
@@ -43,14 +65,26 @@ function Contact() {
     return <>
     <div className="box-contact">
         <h1>Contact</h1>
+        {successMessage && (
+            <p>{successMessage}</p>
+        )}
         <form onSubmit={handleSubmit} className="form-contact">
+
+            {errors.name && errors.name.map((error, index) => (
+                <p key={index} className="error-message">Erreur : {error}</p>
+            ))}
             <label htmlFor="name">Nom :</label>
             <input name="name" type="text" />
             
-
+            {errors.email && errors.email.map((error, index) => (
+                <p key={index} className="error-message">{error}</p>
+            ))}
             <label htmlFor="email">Email :</label>
             <input type="text" name="email" />
 
+            {errors.message && errors.message.map((error, index) => (
+                <p key={index} className="error-message">{error}</p>
+            ))}
             <label htmlFor="message">Message :</label>
             <textarea name="message" id=""></textarea>
 
