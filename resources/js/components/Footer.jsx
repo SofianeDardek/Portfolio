79b9ba9274931a57af8fe51e078dc923fa43efe3
sofiane <sofiane162@hotmail.fr>
2024-11-01 19:52:37 +0100

    Ajout du composant Footer
    - Création d'un composant Footer pour le site
    - Ajout d'un message de copyright : "Réalisé par Sofiane. Tous droits réservés."
    - Mise en place d'une classe CSS pour le style du footer

---
+++ resources/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return <>
+        <footer className="footer-site">
+            <p>Réalisé par Sofiane. Tous droits réservés.</p>
+        </footer>
+    </>
+}
+
+export default Footer;
