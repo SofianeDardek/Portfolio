36cfe55346fdff7aeb39eafbeca51680a57ffa36
sofiane <sofiane162@hotmail.fr>
2024-11-01 20:00:43 +0100

    Ajout d'un tableau de projets
    - Création d'un tableau d'objets pour les projets
    - Permet d'itérer sur les projets ultérieurement à l'aide de la méthode map

--- resources/js/components/Projects.jsx
+++ resources/js/components/Projects.jsx
@@ -1,4 +1,27 @@
 function Projects() {
+
+    // Création d'un tableau "projects" qui regroupe les projets sous forme d'objets
+    const projects = [
+        {
+            id: 1,
+            title: "Projet Restaurant",
+            desc: "Ce projet de site web pour un restaurant a été conçu et développé dans le cadre de ma formation de développeur junior au CNAM.",
+            img: "/images/projects/oburo/oburo_index.png"
+        },
+        {
+            id: 2,
+            title: "Projet Portfolio",
+            desc: "Ce projet a pour but de présenter qui je suis.",
+            img: ""
+        },
+        {
+            id: 3,
+            title: "Site web pour un centre social",
+            desc: "Ce site web a été créé lors de mon stage de fin de première année de BTS.",
+            img: ""
+        }
+    ]
+    
     return (
         <>
             <div className="content-projects">
@@ -6,9 +29,9 @@ function Projects() {
                 <hr />
                 <div className="projects-container">
                     <div className="project-card">
-                        <img src="/images/projet1.jpg" alt="Aperçu du projet 1" className="project-image"/>
-                        <h2>Portfolio Personnel</h2>
-                        <p>Un site web de portfolio pour présenter mes compétences et expériences. Construit avec HTML, CSS, JavaScript, et React.</p>
+                        <img src="/images/projects/oburo/oburo_index.png" alt="Aperçu du projet 1" className="project-image"/>
+                        <h2>Projet Restaurant</h2>
+                        <p>Ce projet de site web pour un restaurant a été conçu et développé dans le cadre de ma formation de développeur junior au CNAM. </p>
                         <a href="https://lien-vers-le-projet.com" target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet</a>
                     </div>
                     <div className="project-card">
