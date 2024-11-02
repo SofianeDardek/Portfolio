d1722e4f99eecc5eb1d0d2baa48e3d19c0058c5f
sofiane <sofiane162@hotmail.fr>
2024-11-02 16:08:59 +0100

    Ajout de fonction store pour insérer les données
    - Ajout du modèle Contact pour enregistrer les données validées dans la base de données.
    - Création d'un enregistrement Contact avec les données validées après la vérification.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
@@ -16,10 +17,16 @@ class ContactController extends Controller
             'message' => 'required|string',
         ]);
 
+
         if ($validator->fails()) {
             // Retourner les erreurs de validation avec un statut 422
             return response()->json(['errors' => $validator->errors()], 422);
         }
+
+            // Récupérer les données validées
+    $validatedData = $validator->validated();
+
+       Contact::create($validatedData);
     
         return response()->json(['message' => 'je suis le controller']);
     }
