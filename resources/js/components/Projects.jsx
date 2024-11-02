bf834835108cce9f71ab814f9ee6eed561c3b0c7
sofiane <sofiane162@hotmail.fr>
2024-11-02 16:04:24 +0100

    Refactorisation du composant "Projects"
    - Utilisation de la méthode `map` pour générer dynamiquement les cartes de projets à partir du tableau `projects`.
    - Ajout des images pour le "Projet Portfolio" et le "Site web pour un centre social".
    - Simplification et amélioration de la maintenabilité du code.

--- resources/js/components/Projects.jsx
+++ resources/js/components/Projects.jsx
@@ -12,13 +12,13 @@ function Projects() {
             id: 2,
             title: "Projet Portfolio",
             desc: "Ce projet a pour but de présenter qui je suis.",
-            img: ""
+            img: "/images/projects/portfolio/portfolio_index.png"
         },
         {
             id: 3,
             title: "Site web pour un centre social",
             desc: "Ce site web a été créé lors de mon stage de fin de première année de BTS.",
-            img: ""
+            img: "/images/projects/school/cs-laruche.png"
         }
     ]
     
@@ -28,24 +28,15 @@ function Projects() {
                 <h1 className="title-page">Mes Projets</h1>
                 <hr />
                 <div className="projects-container">
+                    { projects.map((el) => {
+                    return (
                     <div className="project-card">
-                        <img src="/images/projects/oburo/oburo_index.png" alt="Aperçu du projet 1" className="project-image"/>
-                        <h2>Projet Restaurant</h2>
-                        <p>Ce projet de site web pour un restaurant a été conçu et développé dans le cadre de ma formation de développeur junior au CNAM. </p>
+                        <img src={el.img} alt="Aperçu du projet 1" className="project-image"/>
+                        <h2>{ el.title }</h2>
+                        <p>{el.desc}</p>
                         <a href="https://lien-vers-le-projet.com" target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet</a>
-                    </div>
-                    <div className="project-card">
-                        <img src="/images/projet2.jpg" alt="Aperçu du projet 2" className="project-image"/>
-                        <h2>Application de Gestion de Tâches</h2>
-                        <p>Une application de gestion de tâches développée en Laravel et Vue.js pour organiser le travail en équipe.</p>
-                        <a href="https://lien-vers-le-projet.com" target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet</a>
-                    </div>
-                    <div className="project-card">
-                        <img src="/images/projet3.jpg" alt="Aperçu du projet 3" className="project-image"/>
-                        <h2>Site E-commerce</h2>
-                        <p>Un site e-commerce complet avec intégration de paiement en ligne, réalisé avec PHP et MySQL.</p>
-                        <a href="https://lien-vers-le-projet.com" target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet</a>
-                    </div>
+                    </div>);
+                    }) }
                 </div>
             </div>
         </>
