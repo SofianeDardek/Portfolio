bee4266415e70dc96bf77bd958b5af2134403ed4
sofiane <sofiane162@hotmail.fr>
2024-11-04 19:39:11 +0100

    Amélioration de la navigation
    - Remplacement du lien externe par un `Link` de `react-router-dom` permettant une navigation interne vers une page de détails pour chaque projet.
    - Suppression des descriptions dans la liste des projets (les détails sont désormais disponibles dans les pages de projet spécifiques).
    - Amélioration de l'expérience utilisateur en facilitant l'accès à des informations détaillées sur chaque projet sans rechargement de page.

--- resources/js/components/Projects.jsx
+++ resources/js/components/Projects.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Projects() {
 
     // Création d'un tableau "projects" qui regroupe les projets sous forme d'objets
@@ -5,19 +7,16 @@ function Projects() {
         {
             id: 1,
             title: "Projet Restaurant",
-            desc: "Ce projet de site web pour un restaurant a été conçu et développé dans le cadre de ma formation de développeur junior au CNAM.",
             img: "/images/projects/oburo/oburo_index.png"
         },
         {
             id: 2,
             title: "Projet Portfolio",
-            desc: "Ce projet a pour but de présenter qui je suis.",
             img: "/images/projects/portfolio/portfolio_index.png"
         },
         {
             id: 3,
             title: "Site web pour un centre social",
-            desc: "Ce site web a été créé lors de mon stage de fin de première année de BTS.",
             img: "/images/projects/school/cs-laruche.png"
         }
     ]
@@ -33,8 +32,7 @@ function Projects() {
                     <div className="project-card">
                         <img src={el.img} alt="Aperçu du projet 1" className="project-image"/>
                         <h2>{ el.title }</h2>
-                        <p>{el.desc}</p>
-                        <a href="https://lien-vers-le-projet.com" target="_blank" rel="noopener noreferrer" className="project-link">Voir le projet</a>
+                        <Link to={`/projects/${el.id}`} className="project-link">Voir le projet</Link>
                     </div>);
                     }) }
                 </div>
