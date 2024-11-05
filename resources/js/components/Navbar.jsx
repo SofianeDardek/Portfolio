db1e31588b8154b9ee6ff6dcdbb6fb01d7baa9c2
sofiane <sofiane162@hotmail.fr>
2024-11-05 19:40:57 +0100

    Ajout d'un wrapper pour que le footer reste en bas

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -3,22 +3,24 @@ import Footer from "./Footer";
 
 function Navbar(){
     return <>
-    <header>
-            <nav>
-            <button className="menu-toggle">Menu</button>
-                <ul>
-                    <li><NavLink to="/">Accueil</NavLink></li>
-                    <li><NavLink to="/skills">Compétences</NavLink></li>
-                    <li><NavLink to="/projects">Projets</NavLink></li>
-                    <li><a href="#">Mon CV</a></li>
-                </ul>
-            </nav>
-                <Link to="/contact" className="button-contact">Contact</Link>
-    </header>
-            <div className="outlet-container">
-                <Outlet />
-            </div>
-    <Footer />
+    <div className="wrapper">
+        <header>
+                <nav>
+                <button className="menu-toggle">Menu</button>
+                    <ul>
+                        <li><NavLink to="/">Accueil</NavLink></li>
+                        <li><NavLink to="/skills">Compétences</NavLink></li>
+                        <li><NavLink to="/projects">Projets</NavLink></li>
+                        <li><a href="#">Mon CV</a></li>
+                    </ul>
+                </nav>
+                    <Link to="/contact" className="button-contact">Contact</Link>
+        </header>
+                <div className="outlet-container">
+                    <Outlet />
+                </div>
+        <Footer />
+    </div>
 
     </>
 }
