26f60e064a48d1f9d900a62ca2c7be0f9bd25aa0
sofiane <sofiane162@hotmail.fr>
2024-11-05 19:44:08 +0100

    Refactorisation du composant Skills
    - Récupération des compétences via une API.
    - Ajout d'un état de chargement pour améliorer l'expérience utilisateur.

--- resources/js/components/Skills.jsx
+++ resources/js/components/Skills.jsx
@@ -1,38 +1,51 @@
+import { useEffect, useState } from "react";
+
 function Skills() {
+
+    const [skills, setSkills] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchSkills = async () => {
+
+            try {
+                const response = await fetch("api/skills");
+    
+                if (!response.ok) {
+                    throw new Error("Erreur lors de la récupération des compétences");
+                }
+    
+                
+                const data = await response.json();
+                setSkills(data);
+                console.log(skills);
+                
+            } catch (error) {
+                console.error("Erreur de traitement :", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchSkills();
+    }, []);
     return (
         <>
             <div className="content-skills">
                 <h1 className="title-page">Compétences</h1>
                 <hr />
                 <div className="skills-container">
+               {  loading ? (
+                <p style={{color:"white"}}>chargement...</p>
+               ) : (
+                    skills.map(skill => (
                     <div className="skill">
-                        <h2>HTML</h2>
-                        <p>Maîtrise des balises HTML, structuration de contenu web.</p>
+                        <h2>{skill.name}</h2>
+                        <p>{skill.description}</p>
                     </div>
-                    <div className="skill">
-                        <h2>CSS</h2>
-                        <p>Compétences en mise en page, design responsive et animations.</p>
-                    </div>
-                    <div className="skill">
-                        <h2>JavaScript</h2>
-                        <p>Développement d'interactions dynamiques et manipulation du DOM.</p>
-                    </div>
-                    <div className="skill">
-                        <h2>PHP</h2>
-                        <p>Création de scripts côté serveur, gestion de bases de données.</p>
-                    </div>
-                    <div className="skill">
-                        <h2>SQL</h2>
-                        <p>Gestion de bases de données relationnelles et rédaction de requêtes.</p>
-                    </div>
-                    <div className="skill">
-                        <h2>Laravel</h2>
-                        <p>Développement d'applications web avec le framework PHP Laravel.</p>
-                    </div>
-                    <div className="skill">
-                        <h2>Git</h2>
-                        <p>Gestion de versions et collaboration sur des projets de développement.</p>
-                    </div>
+                    ))
+                )}
+                    
                 </div>
             </div>
         </>
