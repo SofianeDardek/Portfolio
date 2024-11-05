5cd15ce45405b78ce1327f58adae1d929ba1834e
sofiane <sofiane162@hotmail.fr>
2024-11-05 20:39:54 +0100

    Ajout d'une route API
    - Ajout de la route GET `/api/skills` gérée par `SkillsController@index` pour fournir les données de compétences à l'application front-end.
    - Réorganisation des routes pour que les requêtes API soient traitées par Laravel avant la route générique `{any}` de React.
    - Mise à jour de l'ordre des routes pour éviter que les routes API soient interceptées par la route `{any}`.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\SkillsController;
+
+// Routes d'API : On definit d'abord toutes les routes d'API pour s'assurer qu'elles sont traitées par Laravel.
+// Cela permet d'éviter qu'elles soient interceptées par la route générique {any}, qui redirige vers l'application React.
+Route::prefix('api')->group(function () {
+    Route::post('/contact', [ContactController::class, 'store']);
+    Route::get('/skills', [SkillsController::class, 'index']);
+});
+
 
 Route::get('{any}', function () {
     return view('app'); // Renvoie la vue Blade "app.blade.php" où React est monté
@@ -16,6 +25,3 @@ Route::prefix('api')->group(function () {
 */
 
 
-Route::prefix('api')->group(function () {
-    Route::post('/contact', [ContactController::class, 'store']);
-});
