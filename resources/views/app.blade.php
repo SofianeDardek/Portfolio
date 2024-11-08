a4179bc0c2bd00fc6ef53c603598a6e30ccf07b1
sofiane <sofiane162@hotmail.fr>
2024-11-08 19:08:20 +0100

    Ajout des balises meta description et keywords
    - Ajout de la balise meta description pour améliorer le SEO
    - Ajout de la balise meta keywords pour définir les mots-clés associés à mon profil pro
    - Changement de la langue de "en" à "fr"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Sofiane Dardek, 24 ans, Développeur Web passionné, spécialisé en PHP, JavaScript, React, Laravel, SQL, HTML et CSS. Diplômé d'un BTS SNIR, actuellement en licence 3 informatique au CNAM de Lille en alternance. Motivé par l'innovation et l'acquisition de nouvelles compétences techniques." />
+    <meta name="keywords" content="Sofiane Dardek, développeur web, développement web, PHP, JavaScript, React, Laravel, SQL, HTML, CSS, BTS SNIR, licence 3 informatique, alternance, informatique, réseaux, CNAM Lille, développement frontend, développement backend, passionné, innovation, nouvelles tendances" />
     <title>Document</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.jsx'])
