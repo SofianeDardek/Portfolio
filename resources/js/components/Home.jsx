4177b1788273d9e6abde82ba7a5c2fc4666b36a0
sofiane <sofiane162@hotmail.fr>
2024-11-08 10:06:28 +0100

    Modification du contenu
    - Simplification de la présentation pour améliorer la lisibilité et l'expérience utilisateur

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -19,7 +19,13 @@ function Home(){
             
             <div className="title-text-container">
                 <h1>Sofiane Dardek, 24 ans, Développeur Web Passionné</h1>
-                <p>Je m'appelle Sofiane Dardek et je suis passionné par le développement web et l'informatique. Après avoir obtenu mon BTS Systèmes Numériques option Informatique et Réseaux (SNIR), j'ai renforcé mes compétences dans la programmation, les réseaux et les nouvelles technologies. Motivé par l'innovation et les défis techniques, je m'engage à créer des solutions numériques adaptées aux besoins des utilisateurs. Actuellement, je souhaite poursuivre mes études en licence professionnelle en alternance afin de continuer à me former tout en acquérant de l'expérience en entreprise. Ma détermination et ma curiosité me poussent à toujours apprendre et à m'adapter aux nouvelles tendances du secteur.
+                <p>
+                    Je m'appelle Sofiane Dardek et je suis passionné par le développement web et l'informatique. 
+                    Après avoir obtenu mon BTS Systèmes Numériques option Informatique et Réseaux (SNIR), 
+                    j'ai renforcé mes compétences en programmation et en réseaux. 
+                    Motivé par l'innovation et les défis techniques je souhaite poursuivre mes études en licence 3 informatique au CNAM de Lille en alternance, 
+                    afin de continuer à me former tout en acquérant de l'expérience en entreprise. 
+                    Ma détermination et ma curiosité me poussent à toujours apprendre et à m'adapter aux nouvelles tendances du secteur.
                 </p>
             </div>
         </div>
