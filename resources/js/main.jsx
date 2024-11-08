b226893036117ba960430acc3dc27baf53533151
sofiane <sofiane162@hotmail.fr>
2024-11-08 18:55:22 +0100

    Rremplacement de Navbar par Layout
    - Remplacement du composant 'Navbar' par 'Layout'.

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.jsx';
 import { createBrowserRouter, Outlet, RouterProvider, } from "react-router-dom";
-import Navbar from './components/Navbar';
+import Layout from './components/Layout';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
@@ -12,7 +11,7 @@ import ProjectDetails from './components/ProjectDetails';
 const router = createBrowserRouter([
   {
     path : "/",
-    element : <Navbar />,
+    element : <Layout />,
     children : [
       {
         path : "/",
