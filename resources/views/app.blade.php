4601b8814d0aa1bef0cf2004cf401384178b4a87
sofiane <sofiane162@hotmail.fr>
2024-11-11 13:32:09 +0100

    Modification du titre de la page
    - Mise à jour du titre : "Sofiane Dardek - Développeur Web Passionné | Portfolio"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Sofiane Dardek, 24 ans, Développeur Web passionné, spécialisé en PHP, JavaScript, React, Laravel, SQL, HTML et CSS. Diplômé d'un BTS SNIR, actuellement en licence 3 informatique au CNAM de Lille en alternance. Motivé par l'innovation et l'acquisition de nouvelles compétences techniques." />
     <meta name="keywords" content="Sofiane Dardek, développeur web, développement web, PHP, JavaScript, React, Laravel, SQL, HTML, CSS, BTS SNIR, licence 3 informatique, alternance, informatique, réseaux, CNAM Lille, développement frontend, développement backend, passionné, innovation, nouvelles tendances" />
-    <title>Document</title>
+    <title>Sofiane Dardek - Développeur Web Passionné | Portfolio</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.jsx'])
 </head>
